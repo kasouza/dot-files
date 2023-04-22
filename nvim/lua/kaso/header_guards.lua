c06f9a30738cf93872cebd5eecec69d68be73573
kasouza <kaua.scmm@gmail.com>
2023-04-22 11:03:29 -0300

    Using git root as project root for header guards

---
+++ nvim/lua/kaso/header_guards.lua
@@ -0,0 +1,60 @@
+local M = {}
+M.config = {}
+
+-- A pattern to which separators should be replaced by '_' in the final guard
+M.config.separators = '[%.\\/-]'
+
+-- Splits a string (I don't know what else to say here lol)
+local function split(inputstr, sep)
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+
+    return t
+end
+
+-- Joins a table (array) into a string, using `sep` as a separator between the string
+local function join(t, sep)
+    local str = ""
+    for k, v in pairs(t) do
+        str = str .. v .. sep
+    end
+
+    return str:sub(0, #str - 1)
+end
+
+-- Generate the header guards and output then into the current buffer
+function M.header_guards(user_name)
+    -- Uses the folder where nvim was started as a `project_name`
+    local handle = io.popen("git rev-parse --show-toplevel")
+    local result = handle:read("*a")
+    local rc = { handle:close() }
+    rc = rc[3]
+
+    local path = vim.fn.getcwd()
+    if rc == 0 then
+        path = result
+    end
+
+    local cwd = split(path, '\\/')
+
+    -- local cwd = split(vim.fn.getcwd(), '\\/')
+    local project_name = cwd[#cwd]:upper()
+
+    -- Current open file (actually it's the path relative to `cwd`)
+    local filename = vim.fn.expand("%"):upper()
+
+    -- The macro to be used as the header guard
+    local macro = join({ user_name, project_name, filename }, '_'):gsub(M.config.separators, "_")
+
+    -- Create the actual header guards
+    local ifndef = "#ifndef " .. macro
+    local define = "#define " .. macro
+    local endif = "#endif"
+
+    -- Put then in the file (with a empty line between the #define and #endif
+    vim.api.nvim_put({ ifndef, define, '', endif }, 'l', P, true)
+end
+
+return M
--- nvim/lua/kaso/init.lua
+++ nvim/lua/kaso/init.lua
@@ -1,3 +1,4 @@
 require("kaso.set")
 require("kaso.remap")
 require("kaso.packer")
+require("kaso.header_guards")
