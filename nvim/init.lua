ec66d112c4a5ea15ebd5e8f71c6c377a09af97b9
kasouza <kaua.scmm@gmail.com>
2025-01-23 17:44:54 -0300

    Ssaaske

--- nvim/init.lua
+++ nvim/init.lua
@@ -221,6 +221,23 @@ require("dap").configurations.javascript = {
     },
 }
 
+-- Treesitter
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = "https://github.com/EmranMR/tree-sitter-blade",
+    files = {"src/parser.c"},
+    branch = "main",
+  },
+  filetype = "blade"
+}
+
+vim.cmd([[
+augroup BladeFiltypeRelated
+  au BufNewFile,BufRead *.blade.php set ft=blade
+augroup END
+]])
+
 -- END config-plugins
 
 -- START LSP
